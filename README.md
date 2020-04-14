0716c5827058090816b39890ae6e9f2001e11469
NasarTarique <nasartarique@gmail.com>
2020-04-14 20:55:02 +0530

    tweet.py and README update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # POET_BOT 
 A twitter bot that automatically scrapes poems and uploads on twitter .
+
+
+##Setup and Authentication
+* Clone the repo and setup the requirements 
+* Create a twitter devloper account and get the API key,consumer secret, access token , access token secret
+* Create a client_secret.json in the poet_bot directory and put the above data in the json file or just put the data directly in tweet.py 
+* Run python3 tweet.py 
+
+## Screenshots
+![](twitterbot.jpg)
--- tweet.py
+++ tweet.py
@@ -1,15 +1,44 @@
 import tweepy
 import json
-from langdetect import detect
+import get_poem
 
 client = open('client_secret.json',)
-client_dictionary =json.load(client)
-print(client_dictionary)
+client_dictionary = json.load(client)
 auth = tweepy.OAuthHandler(client_dictionary['consumer_key'], client_dictionary['consumer_secret'])
 auth.set_access_token(client_dictionary['access token'],client_dictionary['access token secret'])
 api = tweepy.API(auth)
-'''
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-'''
+
+poem = get_poem.get_poem()
+
+str = poem['poem_title'] +'\n'+'\n'+'by'+poem['author_name'] +'\n' +'\n'
+count_chars = len(poem['poem_title']) + len(poem['author_name'])+ 6
+poemparts = []
+list = poem['poem'].splitlines()
+for x in list:
+    if x==list[len(list)-1]:
+        str = str+'\n'+x
+        poemparts.append(str)
+    if (len(x)+count_chars)<140:
+        str = str+'\n'+x
+        count_chars = count_chars+len(x)
+    else:
+        poemparts.append(str)
+        str=''
+        count_chars=0
+
+
+if len(poemparts)==1:
+    api.update_status(poemparts[0])
+    print("tweeted")
+else:
+    api.update_status(poemparts[0])
+    status_list = api.user_timeline('PoetBot2')
+    tweetid = status_list[0].id
+    for x in range(1,len(poemparts)):
+        status_list = api.user_timeline('PoetBot2')
+        tweetid = status_list[0].id
+        api.update_status(status = poemparts[x], in_reply_to_status_id = tweetid, auto_populate_reply_metadata=True)
+
+
+
+
